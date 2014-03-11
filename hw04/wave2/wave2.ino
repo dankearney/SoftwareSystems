48e9b29520048190ddeead3f5efa9837a4e87fe4
Dan Kearney <daniel.kearney@students.olin.edu>
2014-03-11 19:50:22 -0400

    replaced bitwise with bytewise

--- hw04/wave2/wave2.ino
+++ hw04/wave2/wave2.ino
@@ -33,15 +33,17 @@ void setup() {
   pinMode(8, OUTPUT);  
   pinMode(7, OUTPUT);  
   pinMode(6, OUTPUT);  
+  
 }
 
+int reverse1[] = {
+0, 32, 16, 48, 8, 40, 24, 56, 4, 36, 20, 52, 12, 44, 28, 60, 2, 34, 18, 50, 10, 42, 26, 58, 6, 38, 22, 54, 14, 46, 30, 62, 1, 33, 17, 49, 9, 41, 25, 57, 5, 37, 21, 53, 13, 45, 29, 61, 3, 35, 19, 51, 11, 43, 27, 59, 7, 39, 23, 55, 15, 47, 31, 63, };
+
+int reverse2[] = {
+0, 128, 64, 192, };
+
 void writeByte(int x) {
-  int pin;
-  
-  for (pin=13; pin>=6; pin--) {
-    digitalWrite(pin, x&1);
-    x >>= 1;
-  }
+  PORTB = reverse[x];
 }
 
 int low = 36;
@@ -51,8 +53,10 @@ int counter = low;
 
 void loop() {
   int button1 = digitalRead(buttonPin1);
-  if (button1) return;
-  
+  if(button1) {
+    digitalWrite(ledPin, LOW);
+    return;
+  }
   counter += stride;
   if (counter > high) {
     counter = low;
@@ -61,4 +65,5 @@ void loop() {
 
   // write to the digital pins  
   writeByte(counter);
+  digitalWrite(ledPin, HIGH);
 }
